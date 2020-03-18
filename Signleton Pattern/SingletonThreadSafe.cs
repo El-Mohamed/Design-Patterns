b124538896967099f04d81c00f6b8eca12aff65e
Mohamed <elmoufid.mohamed@outlook.com>
2020-03-18 16:31:27 +0100

    Add ThreadSafe Singleton

---
+++ Signleton Pattern/SingletonThreadSafe.cs
@@ -0,0 +1,34 @@
+namespace Signleton_Pattern
+{
+
+    // Rename it to own class
+    public sealed class Singleton
+    {
+
+        private static volatile Singleton instance;
+        private static object syncRoot = new object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
