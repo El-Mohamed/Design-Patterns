cc0a8a871c8a33ba276db058bd15d9631724589b
Mohamed <elmoufid.mohamed@outlook.com>
2020-03-18 22:39:12 +0100

    Add Car Class

---
+++ State Pattern/Car.cs
@@ -0,0 +1,33 @@
+namespace State_Pattern
+{
+    public class Car
+    {
+        public State stoppedState { get; set; }
+        public State drivingState { get; set; }
+        public State state { get; set; }
+
+        public Car()
+        {
+            stoppedState = new StoppedState(this);
+            drivingState = new DrivingState(this);
+
+            state = stoppedState;
+        }
+
+        public void Drive()
+        {
+            state.Drive();
+        }
+
+        public void Brake()
+        {
+            state.Brake();
+        }
+
+        public void setState(State state)
+        {
+            this.state = state;
+        }
+
+    }
+}
