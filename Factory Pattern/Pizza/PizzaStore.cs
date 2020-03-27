1c68aa1b336fc1163dfe374e7be9a71828a0def9
Mohamed <elmoufid.mohamed@outlook.com>
2020-03-27 15:51:08 +0100

    Add PizzaStore Class

---
+++ Factory Pattern/Pizza/PizzaStore.cs
@@ -0,0 +1,26 @@
+namespace Factory_Pattern
+{
+    public abstract class PizzaStore
+    {
+        public PizzaStore()
+        { 
+		
+		}
+
+        public Pizza OrderPizza(string type)
+        {
+            Pizza pizza;
+
+            pizza = CreatePizza(type);
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+
+            return pizza;
+        }
+
+        protected abstract Pizza CreatePizza(string type);
+    }
+}
